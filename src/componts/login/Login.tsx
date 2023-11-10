415769bbb62a7b651c6e10754171d877af5d11a6
sarath <sarathsarath93366@gmail.com>
2023-11-10 23:23:12 +0530

    deploy time small change 2

--- src/componts/login/Login.tsx
+++ src/componts/login/Login.tsx
@@ -66,7 +66,7 @@ function Login() {
               
               }else if(result.err){
 
-                  message.error("server error");
+                  message.error(   "new server error");
              
                 }else{
 
